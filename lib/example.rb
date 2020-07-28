01d24fed379f2aedf0a58d4ea678c7c68543bc2d
Eric Fiutak <fiutake14@gmail.com>
2020-07-28 00:43:05 +0000

    Automatically backed up by Learn

--- lib/example.rb
+++ lib/example.rb
@@ -1,5 +1,5 @@
 # Use this space to code along with the readme
-def say_hello_world_ten_times
+
 phrase = "Hello World!"
 puts phrase
 puts phrase
@@ -12,9 +12,3 @@ puts phrase
 puts phrase
 puts phrase
 end
-
-def greeting
-  puts "Hello World"
-end
-
-greeting
